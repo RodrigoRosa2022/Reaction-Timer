ee3672f3e4fb3621e9079b2ae609cf0f001a58a3
RodrigoRosa2022 <ronibfr@gmail.com>
2023-09-21 01:11:47 -0300

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="block" v-if="showBlock">
+  Click me
+</div>
+  
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false
+    }
+  },
+  mounted() {
+    console.log("component mounted")
+    setTimeOut(() => {}, this.delay)
+  },
+  updated() {
+
+  }
+
+
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+
+</style>
